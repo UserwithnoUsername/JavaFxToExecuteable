f683aea45b267be14f34d8a6156e231a9520a49f
UserwithnoUsername <thomas.diewald@outlook.de>
2024-07-18 13:02:14 +0200

    Remove login functionality from HelloController
    The code related to login actions, including the FusionAuth client instantiation and the onLoginButtonClick method, have been removed from the HelloController. Correspondingly, the "Login" Button has also been removed from the hello-view.fxml file. These changes simplify the controller and focus on its main purpose.
    
    Removed all Unnecessary Stuff for Creating a Executable in JavaFx

--- src/main/java/com/worstcode/creationkit/HelloController.java
+++ src/main/java/com/worstcode/creationkit/HelloController.java
@@ -4,11 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import io.fusionauth.domain .*;
-import io.fusionauth.domain.api .*;
-import io.fusionauth.domain.api.user .*;
-import io.fusionauth.javaclient .*;
-import io.fusionauth.javaclient.domain .*;
 
 
 public class HelloController {
@@ -16,32 +11,6 @@ public class HelloController {
     private Label welcomeText;
 
 
-    public void onLoginButtonClick(ActionEvent actionEvent) {
-    }
 
-    public void onLoginButtonClick(ActionEvent actionEvent) {
-        FusionAuthClient client = new FusionAuthClient("{FusionAuth-api-key}", "{FusionAuth-URL}", (tenantId));
-
-        LoginRequest request = new LoginRequest("ApplicationId");
-        request.loginId = "email@example.com";
-        request.password = "password";
-
-        try {
-            ClientResponse<LoginResponse, Errors> response = client.login(request);
-
-            if (response.wasSuccessful()) {
-                // login successful
-                // Do something
-
-            } else {
-                // login failed
-                // handle errors
-                System.out.println(response.errorResponse);
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
 
